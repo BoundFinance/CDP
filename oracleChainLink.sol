cd749e6b81b4065f88d01c23533e8e8fbee54416
Bound Finance <140350394+BoundFinance@users.noreply.github.com>
2023-08-29 21:11:05 +0100

    Update oracleChainLink.sol

--- oracleChainLink.sol
+++ oracleChainLink.sol
@@ -1,1 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
 
+// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract TestChainlink {
+  AggregatorV3Interface internal priceFeed;
+
+  constructor(address chainlinkfeed) {
+    // ETH / USD
+    priceFeed = AggregatorV3Interface(chainlinkfeed);
+  }
+
+  function getLatestPrice() public view returns (int) {
+    (
+      uint80 roundID,
+      int price,
+      uint startedAt,
+      uint timeStamp,
+      uint80 answeredInRound
+    ) = priceFeed.latestRoundData();
+    // for ETH / USD price is scaled up by 10 ** 8
+    return price / 1e8;
+  }
+}
+
+interface AggregatorV3Interface {
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int answer,
+      uint startedAt,
+      uint updatedAt,
+      uint80 answeredInRound
+    );
+}
